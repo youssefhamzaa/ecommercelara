5d4bac56415ff23d5e8664b900d997a5d9843195
youssefhamzaa <157299461+youssefhamzaa@users.noreply.github.com>
2024-10-15 22:33:21 +0100

    Commit new version

--- public/index.php
+++ public/index.php
@@ -13,10 +13,5 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$request = Request::capture();
-$response = $app->handle($request);
-$response->send();
-
-$app->terminate($request, $response);
+(require_once __DIR__.'/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
